b7459237d0b8be40c87cfb3dc1539ea26dcfed7b
Kevin Rosenberg <karsonic7283@gmail.com>
2022-02-10 02:47:04 -0500

    Add: CORS for Heroku

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from roll20_helper import make_spell_macro
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://roll20-helper.herokuapp.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
